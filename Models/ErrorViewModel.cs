7bd7e0c511ded5f706efbfc95461969f9208efc4
Dunieski Otano <dunieskior@gmail.com>
2020-06-06 13:25:43 -0400

    Initial check-in of module Memberships

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Memberships.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
